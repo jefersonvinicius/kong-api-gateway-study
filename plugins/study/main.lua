f1986dfc4d9fb2c39df14495b720de37ca12b3c2
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2023-07-11 22:18:34 -0300

    Continue studying lua

--- plugins/study/main.lua
+++ plugins/study/main.lua
@@ -1,32 +1,75 @@
 local todos = require 'todos'
 
+local message = nil
+
+function clear_terminal()
+    print("\x1b[2J\x1b[1;1H")
+end
+
 function add_task_option()
     io.write("Task: ")
     task_text = io.read("*line")
     todos.add(task_text)
+    message = 'Task added'
 end
 
-function list_tasks_option()
-    print("# Task")
-    for index, task in ipairs(todos.all()) do
+function print_tasks_table(tasks) 
+    print("\n\n# Task")
+    if (#tasks == 0) then
+        print "Empty"
+        return
+    end
+    for index, task in ipairs(tasks) do
         print(task.id .. " " .. task.text)
     end
+end 
+
+function list_tasks_option()
+    print_tasks_table(todos.all())
+    io.write("\nPress any key to continue...")
+    io.read()
+end
+
+function delete_tasks_option()
+    tasks = todos.all()
+    if (#tasks == 0) then
+        message = 'Tasks not found'
+        return
+    end
+    print_tasks_table(todos.all())
+    io.write('\nTask id: ')
+    task_id = tonumber(io.read("*line"))
+    if (todos.exists(task_id) == false) then
+        message = 'Task not found'
+        return
+    end
+    todos.delete(task_id)
+    message = 'Task deleted'
 end
 
 operations = {
-    ["1"]=add_task_option,
-    ["2"]=list_tasks_option
+    {label = "Add task", handler = add_task_option},
+    {label = "List tasks", handler = list_tasks_option},
+    {label = "Delete tasks", handler = delete_tasks_option}
 }
 
+
 while (true) do
-    print("[1] Add task")
-    print("[2] List tasks")
+    clear_terminal()
+    print("TODOLISTAPP")
+    if (message ~= nil) then
+        print("\n>> " .. message .. " <<\n")
+        message = nil
+    end
+    for index, operation in ipairs(operations) do
+        print("[" .. index .. "] " .. operation.label)
+    end
     io.write(": ")
-    option = io.read("*line")
-    operation = operations[option] 
+    option = tonumber(io.read("*line"))
+    operation = operations[option]
     if (operation == nil) then
-        print("Invalid option")
+        message = 'Invalid option'
     else
-        operation()
+        operation.handler()
     end
 end
--- plugins/study/todos.lua
+++ plugins/study/todos.lua
@@ -3,13 +3,21 @@ local current_id = 1
 
 function todos.add(task_text)
     todo = {id=current_id,text=task_text}
-    table.insert(todos, todo)
+    todos[todo.id] = todo
     current_id = current_id + 1
     return todo
 end
 
 function todos.all()
     return todos
+end
+
+function todos.exists(task_id) 
+    return todos[task_id] ~= nil
+end
+
+function todos.delete(task_id)
+    table.remove(todos, task_id)
 end
 
 return todos
